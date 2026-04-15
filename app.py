8176616379e5e612c755c63f81a9555b3b03c09c
Morales Leon <citemaderalima19@itp.gob.pe>
2026-04-15 17:27:43 -0500

    fix render

--- app.py
+++ app.py
@@ -882,6 +882,9 @@ def partidos_hoy():
         )
 
 
+import os
+
 if __name__ == "__main__":
     init_cache_db()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
